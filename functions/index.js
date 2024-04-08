a44bb855c95b9e6e95579b9330e0ebc1632a7cee
Jose Luiz Rodrigues <zeluizr@me.com>
2024-04-08 19:47:59 -0400

    Refactor createUser function to registerUser in index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 const { helloWorld } = require("./src/functions/helloWorld");
-const { createUser } = require("./src/functions/createUser");
+const { registerUser } = require("./src/functions/createUser");
 
 exports.helloWorld = helloWorld;
-exports.createUser = createUser;
+exports.createUser = registerUser;
--- functions/src/functions/createUser.js
+++ functions/src/functions/createUser.js
@@ -5,11 +5,6 @@ const cors = require("cors")({ origin: true }); // Habilita o CORS para todas as
 
 exports.registerUser = functions.https.onRequest((req, res) => {
   cors(req, res, async () => {
-    if (req.method !== "POST") {
-      return res.status(405).send({ error: "Método não permitido" });
-    }
-
-    // Seu código para criar um usuário aqui
     try {
       const { email, password } = req.body;
       const userRecord = await admin.auth().createUser({
@@ -19,7 +14,7 @@ exports.registerUser = functions.https.onRequest((req, res) => {
       res.status(200).send({ userId: userRecord.uid });
     } catch (error) {
       console.error("Erro ao registrar usuário:", error);
-      res.status(500).send({ error: "Erro ao criar usuário" });
+      res.status(500).json({ error: "Erro ao criar usuário" });
     }
   });
 });
