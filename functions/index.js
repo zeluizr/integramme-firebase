a35bd0f81a8794d3668d79e605b95c4260395166
Jose Luiz Rodrigues <zeluizr@me.com>
2024-04-08 19:02:38 -0400

    Remove unused files and configurations

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-const { webhook } = require("./src/webhook/webhook.js");
-const { createUser } = require("./src/user/create.js");
-
-exports.webhook = webhook;
-exports.createUser = createUser;
