c42874f77061c6f1447e527835e82f8e3b8f249a
Jose Luiz Rodrigues <zeluizr@me.com>
2024-04-08 20:15:49 -0400

    Refactor createUser function to registerUser in index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 const { helloWorld } = require("./src/functions/helloWorld");
-const { registerUser } = require("./src/functions/createUser");
+const { createUser } = require("./src/functions/createUser");
 
 exports.helloWorld = helloWorld;
-exports.createUser = registerUser;
+exports.registerUser = createUser;
--- functions/src/functions/createUser.js
+++ functions/src/functions/createUser.js
@@ -1,20 +1,23 @@
-// functions/src/auth/registerUser.js
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const cors = require("cors")({ origin: true }); // Habilita o CORS para todas as origensclear
 
-exports.registerUser = functions.https.onRequest((req, res) => {
-  cors(req, res, async () => {
-    try {
-      const { email, password } = req.body;
-      const userRecord = await admin.auth().createUser({
-        email,
-        password,
-      });
-      res.status(200).send({ userId: userRecord.uid });
-    } catch (error) {
-      console.error("Erro ao registrar usuário:", error);
-      res.status(500).json({ error: "Erro ao criar usuário" });
-    }
-  });
+exports.createUser = functions.https.onCall(async (data, context) => {
+  const { email, password } = data;
+
+  try {
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+    });
+
+    // Retorna o UID do usuário recém-criado para o cliente
+    return { userId: userRecord.uid };
+  } catch (error) {
+    console.error("Erro ao registrar usuário:", error);
+    throw new functions.https.HttpsError(
+      "internal",
+      "Não foi possível criar o usuário.",
+      error.message
+    );
+  }
 });
